297cf1bc730d598c7a5fefa81ea09f0b45830685
21B030720 <98741201+21B030720@users.noreply.github.com>
2022-02-02 22:34:31 +0600

    Update lab1_10.py

--- lab1_10.py
+++ lab1_10.py
@@ -1,7 +1,7 @@
-a = str(input())
-l = a.split(" ")
-b = ""
-for i in range(len(l)):
+a = str(input()) # Ввод строки
+l = a.split(" ") # Присвоение слов строки к листу
+b = "" # Результат 
+for i in range(len(l)): # Логика
     if len(l[i]) >= 3:
         b += l[i] + " "
-print(b)
+print(b) # вывод результата
