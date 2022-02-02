da4f0785001c3724fea8684fa022e7d0be6737f5
21B030720 <98741201+21B030720@users.noreply.github.com>
2022-02-02 22:32:11 +0600

    Update lab1_9.py

--- lab1_9.py
+++ lab1_9.py
@@ -1,10 +1,10 @@
-l = []
-a = int(input())
-for i in range(a):
+l = [] # лист, содержащий все адресы
+a = int(input()) # ввод числа адресов
+for i in range(a): # логика отбора номеров
     s = str(input())
     if s.count("@gmail.com"):
         pos = int(s.find("@gmail.com"))
         s = s[0: pos]
         l.append(s)
-for i in range(len(l)):
+for i in range(len(l)): # вывод всех подходящих адресов
     print(l[i])
